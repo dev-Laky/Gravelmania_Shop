1c2347205e2abca1b9d713c33214a1e665e1b7ca
dev-Laky <75489200+dev-Laky@users.noreply.github.com>
2023-04-03 16:34:55 +0200

    update: first checkout draft

---
+++ assets/js/checkout/pre_check_data.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict'
+  
+    document.querySelector('.formular-btn').addEventListener('click', (e) => {
+        // Do whatever you want
+        window.location.href = 'checkout.html';
+      });
+
+  })()
