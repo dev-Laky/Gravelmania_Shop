f39152478711fc74f9a9d7ffe51089992d8826be
dev-Laky <75489200+dev-Laky@users.noreply.github.com>
2023-04-08 00:21:54 +0200

    update: check_for_cart.js simplified

--- assets/js/checkout/check_for_cart.js
+++ assets/js/checkout/check_for_cart.js
@@ -1,12 +1,11 @@
-import { check_for_localstorage } from "/assets/js/cart/cart.js";
+import { get_cart_len } from "/assets/js/cart/cart.js";
 
 (function () {
     'use strict'
 
-    check_for_localstorage();
     // check for empty cart --> redirect to home
-    const shop_cart = JSON.parse(localStorage.getItem('shop_cart'));
-    if(shop_cart.cart.length === 0){
+    const cart_len = get_cart_len();
+    if(cart_len === 0){
         window.location.href = '/';
         throw Error("Cart is Empty. Cannot checkout.");
     }
