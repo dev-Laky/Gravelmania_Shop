7fb6d5bd167e83ba6ea448516e0934482d049a2f
dev-Laky <75489200+dev-Laky@users.noreply.github.com>
2023-04-09 21:47:35 +0200

    update: shopping cart product style on mobile

--- assets/js/cart/shopping_cart.js
+++ assets/js/cart/shopping_cart.js
@@ -20,20 +20,20 @@ import { del_product } from "/assets/js/cart/cart.js";
                 const productDiv = document.createElement('div');
                 productDiv.classList.add('row', 'd-flex', 'justify-content-between', 'align-items-center');
                 productDiv.innerHTML = `
-        <div class="col-md-2 col-lg-2 col-xl-2">
+        <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2">
           <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img7.webp" class="img-fluid rounded-3" alt="${item.name}">
         </div>
-        <div class="col-md-3 col-lg-3 col-xl-3">
+        <div class="col-4 col-sm-3 col-md-3 col-lg-3 col-xl-3 mobile-shopping-cart-text">
           <h6 class="text-muted">Größe ${item.size}</h6>
           <h6 class="text-black mb-0">${item.name}</h6>
         </div>
-        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+        <div class="col-4 col-sm-3 col-md-3 col-lg-2 col-xl-2 offset-lg-1 mobile-shopping-cart-text">
           <h6 class="mb-0">${item.quantity} Mal</h6>
         </div>
-        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+        <div class="col-4 col-sm-3 col-md-3 col-lg-2 col-xl-2 offset-lg-1 mobile-shopping-cart-text">
           <h6 class="mb-0">€ ${item.price}</h6>
         </div>
-        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+        <div class="col-12 col-sm-1 col-md-1 col-lg-1 col-xl-1 text-end">
           <button id="${item.id}" class="btn del-cart-btn"><i class="bi bi-trash text-danger"></i></button>
         </div>
         <hr class="my-4">
