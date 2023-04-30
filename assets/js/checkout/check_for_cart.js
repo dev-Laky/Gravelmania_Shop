a7439d1c2788a77c8ac5f3d3c6a499bfa17c66f7
dev-Laky <75489200+dev-Laky@users.noreply.github.com>
2023-04-30 13:34:02 +0200

    update: all paths changed --> except footer paths

--- assets/js/checkout/check_for_cart.js
+++ assets/js/checkout/check_for_cart.js
@@ -6,7 +6,7 @@ import { get_cart_len } from "../cart/cart.js";
     // check for empty cart --> redirect to home
     const cart_len = get_cart_len();
     if(cart_len === 0){
-        window.location.href = '/';
+        window.location.href = '../';
         throw Error("Cart is Empty. Cannot checkout.");
     }
 
